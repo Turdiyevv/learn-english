31324476f68509c5d9189709e5af34a98c549098
Turdiyev <sherbekturdiyev98@gmail.com>
2025-01-25 19:50:55 +0500

    flags panel done

--- pages/singlComponents/flags.vue
+++ pages/singlComponents/flags.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
-
+import English from "@/assets/images/flags/English.jpg.webp"
+import Spanish from "@/assets/images/flags/Spanish.jpg.webp"
+import French from "@/assets/images/flags/French.jpg.webp"
+import German from "@/assets/images/flags/German.jpg.webp"
+import Italian from "@/assets/images/flags/Italian.jpg.webp"
+import Portuguese from "@/assets/images/flags/Portuguese.jpg.webp"
+import Japanese from "@/assets/images/flags/Japanese.jpg.webp"
+import Chinese from "@/assets/images/flags/Chinese.jpg.webp"
+import Arabic from "@/assets/images/flags/Arabic.jpg.webp"
+import Dutch from "@/assets/images/flags/Dutch.jpg.webp"
+import Korean from "@/assets/images/flags/Korean.jpg.webp"
+import Swedish from "@/assets/images/flags/Swedish.jpg.webp"
+import Norwegian from "@/assets/images/flags/Norwegian.jpg.webp"
+import Greek from "@/assets/images/flags/Greek.jpg.webp"
+import Hebrew from "@/assets/images/flags/Hebrew.jpg.webp"
+import More from "@/assets/images/flags/More-1.jpg.webp"
+const flags = [
+  {name: 'English', src: English},
+  {name: 'Spanish', src: Spanish},
+  {name: 'French', src: French},
+  {name: 'German', src: German},
+  {name: 'Italian', src: Italian},
+  {name: 'Portuguese', src: Portuguese},
+  {name: 'Japanese', src: Japanese},
+  {name: 'Chinese', src: Chinese},
+  {name: 'Arabic', src: Arabic},
+  {name: 'Dutch', src: Dutch},
+  {name: 'Korean', src: Korean},
+  {name: 'Swedish', src: Swedish},
+  {name: 'Norwegian', src: Norwegian},
+  {name: 'Greek', src: Greek},
+  {name: 'Hebrew', src: Hebrew},
+  {name: 'More-14', src: More},
+]
 </script>
 
 <template>
- <div class="border border-gray-200 h-[409px]">
-   <h1>flags</h1>
+ <div class="border border-gray-200 min-h-[410px]">
+   <h2>Which language do you want to learn?</h2>
+   <div class="flex flex-wrap justify-center gap-14 p-5">
+     <div v-for="x in flags" class="cursor-pointer flex flex-wrap items-center justify-center w-[90px] h-[90px] rounded-[20px] ">
+      <img :src="x.src" :alt="x.name" class="border rounded-[20px] hover:w-[90px] hover:h-[90px] transition-all duration-500 w-[78px] h-[78px]">
+      <div>{{x.name}}</div>
+     </div>
+   </div>
  </div>
 </template>
 
--- pages/singlComponents/getStarted.vue
+++ pages/singlComponents/getStarted.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-   <div class="flex flex-wrap items-center h-[408px]">
-     <div class="md:w-1/2 border border-gray-500 p-2">
+   <div class="flex flex-wrap items-center min-h-[430px]">
+     <div class="md:w-1/2 p-2">
        <h3>Talkpal is a GPT-powered AI language teacher. Boost your speaking, listening, writing, and pronunciation skills – Learn 5x Faster!</h3>
         <button class="bg-blue-700 hover:bg-blue-600 rounded-xl h-10 px-3 text-white">get started</button>
      </div>
-     <div class="md:w-1/2 border border-gray-500 p-2">
-       <div class=" border rounded-xl m-2">
-         <img class="rounded-xl h-[354px]" src="@/assets/images/Desktop_Landing_Page.jpg" alt="">
+     <div class="md:w-1/2 p-2">
+       <div class=" rounded-2xl m-2 cursor-pointer">
+         <img class="rounded-2xl  hover:w-[500px] hover:h-[384px] transition-all duration-300 w-[472px] h-[354px]" src="@/assets/images/Desktop_Landing_Page.jpg" alt="">
        </div>
      </div>
    </div>
